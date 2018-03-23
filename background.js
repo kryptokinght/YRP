3ad6aba46e8e733f1d93f3821addd157ff6fcaad
Mahesh Tudu <mahesh86701997@gmail.com>
2018-03-24 04:41:07 +0530

    Added side-bar

--- background.js
+++ background.js
@@ -1,10 +1,16 @@
 console.log("Background JS has loaded");
 
-chrome.browserAction.onClicked.addListener(function(tab){
+/*chrome.browserAction.onClicked.addListener(function(tab){
 	let message = {
 		data: "Hii there"
 	}
 	chrome.tabs.sendMessage(tab.id, message, function(response){
 		console.log(response);
 	});
+});*/
+
+chrome.browserAction.onClicked.addListener(function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id,"toggle");
+    })
 });
