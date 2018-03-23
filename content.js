30658863c6336db68d8b6b2142f729af3f3bc9fb
Mahesh Tudu <mahesh86701997@gmail.com>
2018-03-24 04:58:12 +0530

    Added side-bar

--- content.js
+++ content.js
@@ -10,22 +10,45 @@ chrome.runtime.onMessage.addListener(function(msg, sender){
     if(msg == "toggle"){
         toggle();
     }
-})
+});
 
-var iframe = document.createElement('iframe'); 
-iframe.style.background = "green";
-iframe.style.height = "100%";
-iframe.style.width = "0px";
-iframe.style.position = "fixed";
-iframe.style.top = "0px";
-iframe.style.right = "0px";
-iframe.style.zIndex = "9000000000000000000";
-iframe.frameBorder = "none";
-iframe.style.transition = "0.5s";
-iframe.style.opacity = "0.6";
-iframe.src = chrome.extension.getURL("popup.html")
+var iframe,iframe1;
 
-document.body.appendChild(iframe);
+function createside(){
+	iframe = document.createElement('iframe'); 
+	iframe.style.background = "green";
+	iframe.style.height = "100%";
+	iframe.style.width = "0px";
+	iframe.style.position = "fixed";
+	iframe.style.top = "0px";
+	iframe.style.right = "0px";
+	iframe.style.zIndex = "9000000000000000000";
+	iframe.frameBorder = "none";
+	iframe.style.transition = "0.5s";
+	iframe.style.opacity = "0.8";
+	iframe.src = chrome.extension.getURL("popup.html");
+
+	document.body.appendChild(iframe);	
+}
+
+function createframe(){
+	iframe = document.createElement('iframe'); 
+	iframe.style.background = "green";
+	iframe.style.height = "100%";
+	iframe.style.width = "0px";
+	iframe.style.position = "fixed";
+	iframe.style.top = "0px";
+	iframe.style.right = "0px";
+	iframe.style.zIndex = "9000000000000000000";
+	iframe.frameBorder = "none";
+	iframe.style.transition = "0.5s";
+	iframe.style.opacity = "0.8";
+	iframe.src = chrome.extension.getURL("popup.html")
+
+	document.body.appendChild(iframe);	
+}
+
+createside();
 
 function toggle(){
     if(iframe.style.width == "0px"){
