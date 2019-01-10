30ef355afb6d3a323853b41311162db3c3d16701
Minanshu Singh <minanshu.1998@gmail.com>
2019-01-10 23:32:43 +0530

    update eslint packages, .eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
+    jest: true
   },
   extends: 'eslint:recommended',
   parser: 'babel-eslint',
@@ -16,6 +17,24 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    "no-console": "off",
+    "no-underscore-dangle": "off",
+    "no-plusplus": "off",
+    "no-continue": "off",
+    "camelcase": "off",
+    "no-empty": "off",
+    "no-param-reassign": "off",
+    "func-names": [
+      "error",
+      "never"
+    ],
+    "prefer-destructuring": [
+      "error",
+      {
+        "object": false,
+        "array": false
+      }
+    ],
     indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single', { allowTemplateLiterals: true }],
